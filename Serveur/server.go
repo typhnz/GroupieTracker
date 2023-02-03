abb9eae982d7f91fba57bcdc4d88e261cd58b560
chamen2209 <charles.menard@ynov.com>
2023-02-03 11:44:52 +0100

    add serveur

--- Serveur/server.go
+++ Serveur/server.go
@@ -6,11 +6,12 @@ import (
 )
 
 func main() {
+	fmt.Println("The serveur start on port 8080 🔥")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Bienvenue sur mon serveur Go!")
 	})
 
-	err := http.ListenAndServe(":9090", nil)
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println(err)
 	}
