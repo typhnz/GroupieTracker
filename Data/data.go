0c0894e75e4ec160733b3b4ebe66358bec9903a7
Tayvadi <tayvadi.nzonza@ynov.com>
2023-02-03 09:56:48 +0100

    json

--- Data/data.go
+++ Data/data.go
@@ -1,9 +1,9 @@
 package data
 
-type Artist struct {
-	ID 
-	Image
-	Name
-	Members
-	ReleaseDate
+type artist struct {
+	ID          string `json:"id"`
+	Image       string `json:"image"`
+	Name        string `json:"name"`
+	Members     int    `json:"members"`
+	ReleaseDate string `json:"releasedate"`
 }
