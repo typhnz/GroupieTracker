beb1f3c9c82c72c757507429fae0cc3e6f49c29c
chamen2209 <charles.menard@ynov.com>
2023-02-09 12:01:36 +0100

    data

--- Data/data.go
+++ Data/data.go
@@ -1,10 +1,10 @@
-package data
+package main
 
 import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"os"
+	"net/http"
 )
 
 type artist struct {
@@ -16,14 +16,89 @@ type artist struct {
 }
 
 type dates struct {
-
 	ID int `json:"id"`
-	Dates []string `json:"dates"`
-
+	Date []string `json:"dates"`
 }
 
 
-type locations struct {
+type location struct {
 	ID int `json:"id"`
 	Locations []string `json:"locations"`
 }
+
+type relations struct {
+	ID int `json:"id"`
+	Relations []string `json:"relations"`
+}
+
+func main() {
+	artists()
+}
+
+func artists() {
+	var a artist
+	url := "https://groupietrackers.herokuapp.com/api/artists"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	err := json.Unmarshal((body), &a)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Personne:", a)
+}
+
+/*func dat() {
+	var d dates
+	url := "https://groupietrackers.herokuapp.com/api/dates"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	err := json.Unmarshal((body), &d)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Dates:", d)
+}
+
+func locations() {
+	var l locations
+	url := "https://groupietrackers.herokuapp.com/api/locations"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	err := json.Unmarshal((body), &l)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Locations:", l)
+}
+
+func relations() {
+	var r relations
+	url := "https://groupietrackers.herokuapp.com/api/relations"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	err := json.Unmarshal((body), &r)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Relations:", r)
+}
+
+func main() {
+	artists()
+	dates()
+	locations()
+}
+
+*/
