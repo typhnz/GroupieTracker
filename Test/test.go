b4a560577cce9abf934dd82b0f9001762c1786ac
chamen2209 <charles.menard@ynov.com>
2023-02-09 16:57:12 +0100

    test

--- Test/test.go
+++ Test/test.go
@@ -16,7 +16,7 @@ type artist struct {
 	FirstAlbum string
 	Locations string
 	ConcertDates string
-	Relation string
+	//Relation []string
 }
 
 func main() {
@@ -35,5 +35,16 @@ func artists() {
 		fmt.Println("Error:", err)
 		return
 	}
-	fmt.Println("Personne:", a[0].Relation)
+	fmt.Println("Image:", a[0].Image)
+	fmt.Println("Nom:", a[0].Name)
+	fmt.Println("Membres:", a[0].Members)
+	fmt.Println("Date of creation:", a[0].CreationDate)
+	fmt.Println("Date of first Album:", a[0].FirstAlbum)
+	fmt.Println("Location:", a[0].Locations)
+	fmt.Println("Date of concert:", a[0].ConcertDates)
+
+
+
 }
+
+
