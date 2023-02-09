2695a2480753235d7b3bd7d382d7f8d5867861f6
stom <tom.saillard@ynov.com>
2023-02-09 10:29:51 +0100

    Do the server and now we have home page + a second one, it's normally link with the HTML and CSS now

--- Server/server.go
+++ Server/server.go
@@ -3,13 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
+func homePage(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "homePage")
+}
+
+func contactPage(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "contactPage")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	t , err := template.ParseFiles("./templates/" + tmpl + ".html")
+	if err != nil {
+		fmt.Println(err)
+	}
+	t.Execute(w, nil)
+}
+
 func main() {
-	fmt.Println("The serveur start on port 8080 🔥")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Bienvenue sur mon serveur Go!")
-	})
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/contact", contactPage)
+	http.ListenAndServe(":8080", nil)
+	fmt.Print("The serveur start on port 8080 🔥")
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
