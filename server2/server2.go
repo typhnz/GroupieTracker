1705e046bf643b209b446a66d151ca4e397eea1c
chamen2209 <charles.menard@ynov.com>
2023-02-15 15:32:58 +0100

    Revert "xRevert "test files""
    This reverts commit 6aab44b62ac5fbd0f7f6af83b81b689856d516fb.

---
+++ server2/server2.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	g "groupie/GroupieTracker"
+	"html/template"
+	"net/http"
+)
+
+const port = ":8080"
+
+func home(w http.ResponseWriter, r *http.Request) {
+	type test struct {
+		Name string
+	}
+	var Test test //creating an instance
+	Test.Name = "rémi" //defining a variable in this instance to use for the template
+	tmpl := template.Must(template.ParseFiles("./Templates/index.html")) //fetching the template html
+
+	tmpl.Execute(w, Test) //executing template with the data
+
+	var element g.Artist
+	g.ArtistFunction(element)
+	fmt.Println(element)
+
+	//renderTemplate(w, g.artists(&element))
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "contact")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	/*t, err := template.ParseFiles("../templates/" + tmpl + ".page.tmpl")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w, element)*/
+}
+
+func main() {
+	fmt.Println("(http://localhost:8080) - The serveur start on port", port)
+	templates := http.FileServer(http.Dir("../templates/cssFile"))
+	http.Handle("/cssFile/", http.StripPrefix("/cssFile/", templates))
+	http.HandleFunc("/", home)
+	http.HandleFunc("/contact", contact)
+	http.ListenAndServe(":8080", nil)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
