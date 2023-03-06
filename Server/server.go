73ddc8fa2bdff96953e0ece72b01dfbcdbc24868
chamen2209 <charles.menard@ynov.com>
2023-03-06 11:05:00 +0100

    envoi pour tom

--- Server/server.go
+++ Server/server.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path"
 	"strconv"
 	"text/template"
 )
@@ -49,43 +48,57 @@ const port = ":8080"
 
 var apiElements []ArtistAPI
 
+var artistsData ArtistsArray
+
 func details(w http.ResponseWriter, r *http.Request) {
-	pathID := r.URL.Path
-	pathID = path.Base(pathID)
-	pathIDint, _ := strconv.Atoi(pathID)
-	var locationsObject Relation
+	az := r.FormValue("Oui")
+	fmt.Println(az)
+	id, _ := strconv.Atoi(az)
+	fmt.Println(artistsData.Artists[id-1])
+	// pathID := r.URL.Path
+	// pathID = path.Base(pathID)
+	// pathIDint, _ := strconv.Atoi(pathID)
+	// var locationsObject Relation
 
-	dataArtists := Description{
-		ID:           apiElements[pathIDint-1].ID,
-		Image:        apiElements[pathIDint-1].Image,
-		Members:      apiElements[pathIDint-1].Members,
-		CreationDate: apiElements[pathIDint-1].CreationDate,
-		FirstAlbum:   apiElements[pathIDint-1].FirstAlbum,
-		Locations:    apiElements[pathIDint-1].Locations,
-		ConcertDates: apiElements[pathIDint-1].ConcertDates,
-		Relations:    apiElements[pathIDint-1].Relations,
-	}
-
-	relations, err := http.Get(dataArtists.Relations)
-
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-
-	relationsData, err := ioutil.ReadAll(relations.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	json.Unmarshal(relationsData, &locationsObject)
-
-	// mapInt := map[string]interface{}{
-	// 	"DataArtists": dataArtists,
-	// 	"Relations":   locationsObject,
+	// dataArtists := Description{
+	// 	ID:           apiElements[pathIDint-1].ID,
+	// 	Image:        apiElements[pathIDint-1].Image,
+	// 	Members:      apiElements[pathIDint-1].Members,
+	// 	CreationDate: apiElements[pathIDint-1].CreationDate,
+	// 	FirstAlbum:   apiElements[pathIDint-1].FirstAlbum,
+	// 	Locations:    apiElements[pathIDint-1].Locations,
+	// 	ConcertDates: apiElements[pathIDint-1].ConcertDates,
+	// 	Relations:    apiElements[pathIDint-1].Relations,
 	// }
 
-	renderTemplate(w, "details", nil)
+	// relations, err := http.Get(dataArtists.Relations)
+
+	// if err != nil {
+	// 	fmt.Print(err.Error())
+	// 	os.Exit(1)
+	// }
+
+	// relationsData, err := ioutil.ReadAll(relations.Body)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// json.Unmarshal(relationsData, &locationsObject)
+
+	// // mapInt := map[string]interface{}{
+	// // 	"DataArtists": dataArtists,
+	// // 	"Relations":   locationsObject,
+	// // }
+	
+	//var locationsObject Relation
+
+
+	fmt.Println(artistsData.Artists[id-1].Relations)
+	//fmt.Println(locationsObject)
+	renderTemplate(w, "details", artistsData.Artists[id-1])
+	renderTemplate(w, "details", artistsData.Artists[id-1].Relations)
+
+	//json.Unmarshal([]byte(artistsData.Artists[id-1].Relations), &locationsObject)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -107,9 +120,7 @@ func artist(w http.ResponseWriter, r *http.Request) {
 
 	json.Unmarshal(apiData, &apiElements)
 
-	artistsData := ArtistsArray{
-		Artists: apiElements,
-	}
+	artistsData.Artists = apiElements
 
 	renderTemplate(w, "artist", artistsData)
 }
